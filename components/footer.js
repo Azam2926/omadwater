96cbb6dbffed6d92dec448c166d62bbba52ec9bd
azam <azam.inoyatov.1998@gmail.com>
2021-11-11 23:18:57 +0500

    fixed footer svg margin

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
-      <svg className={'fill-current text-footer'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 184">
+      <svg className='fill-current text-footer -m-0.5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 184">
         <g>
           <path className="fill-current" d="M1285.4,81.2c-34,12.5-98.3,33.1-186.9,39.8c-230.8,17.6-274.8-85-472.8-71.3 c-182,12.6-219.4,104.5-345.3,65.2C237.9,101.6,177,70.7,91.6,81.2c-31.7,4-62.6,13-91.6,26.6V184h1440V0 C1392.2,33.7,1340.3,61,1285.4,81.2z"/>
         </g>

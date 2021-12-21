43bd326ba94cf80dc984ac9aa85eacdae0ad22b4
azam <azam.inoyatov.1998@gmail.com>
2021-12-21 22:23:31 +0500

    SOME FIXES

--- components/Input.js
+++
@@ -1,8 +0,0 @@
-export default function Input(props) {
-    return (
-        <input
-            className={'focus:ring-2 transition-all duration-200 hover:ring-2 ring-blue-500 outline-none py-3 px-6 border rounded-full mx-auto w-full'}
-            {...props}
-        />
-    );
-}

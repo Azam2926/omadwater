8b88c153c3b20ce35e228821e0964396f0e8adf2
Azam <azam.inoyatov.1998@gmail.com>
2022-05-03 13:44:15 +0500

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -62,7 +62,7 @@ export default function Navbar() {
             </div>
             <div className="flex">
               <a href="tel:+998939652829" className='flex flex-wrap items-center justify-center content-center my-3'>
-                <p className="mr-2 font-bold">+998 99 0981375</p>
+                <p className="mr-2 font-bold">+998 93 9652829</p>
                 <p>Bizga qo'ng'iroq qiling</p>
               </a>
             </div>
